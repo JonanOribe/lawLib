b7814e4db9e85a70fb357a818c94ef0a95d997ec
Jon Ander Oribe <jonan.oribe@gmail.com>
2023-01-28 12:07:17 +0100

    Refractor

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ import configparser
 
 config = configparser.ConfigParser()
 config.read("config.ini")
-case_ids:List = [str(i) for i in [*range(1,301)]]#['1','2','29174']
-url:str = config['URLS']['SpanishSupremeCourt']
+case_ids:List = [str(i) for i in [*range(1,5)]]
+url:str = 'SpanishSupremeCourt'
+
+#Something like data/json_data.json
 output_path:str = config['EXTRA']['OutputPath']
 
 #Get some cases
-#returned_data = DataScrapper(url,case_ids).get_data()
-#print(returned_data)
+returned_data = DataScrapper(url,case_ids).get_data()
+print(returned_data)
 
 #Save cases as JSON
 DataScrapper(url,case_ids).save_data('json',output_path)
--- scrapper/data_scrapper.py
+++ scrapper/data_scrapper.py
@@ -1,24 +1,28 @@
 from typing import List
 import requests
 import json
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
 
 class DataScrapper:
-    def __init__(self, url, case_ids) -> None:
-        self.url:str = url
+    def __init__(self, source, case_ids) -> None:
+        self.url:str = {section: dict(config.items(section)) for section in config.sections()}['URLS'][source.lower()]
         self.case_ids:List = case_ids
 
     def get_data(self):
       completed_response = []
       for case_id in self.case_ids:
         completed_url:str = self.url+case_id
-        payload={}
         headers = {
           'Content-Type': 'application/json'
         }
 
-        response = requests.request("GET", completed_url, headers=headers, data=payload)
+        response = requests.request("GET", completed_url, headers=headers, data={})
 
         completed_response.append(response.json())
+
         if (int(case_id) % 100 == 0):
           print('Number of scrapped elements: '+case_id)
 
