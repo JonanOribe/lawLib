7d2a7f4a7702ab217e3214a6e4f125ebd4896133
Jon Ander Oribe <jonan.oribe@gmail.com>
2023-01-28 13:36:03 +0100

    Refractor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from typing import List
-from scrapper import SupremeCourtUnitedStates
+from scrapper import SupremeCourtUnitedStates, SupremeCourtSpain
 import configparser
-
-from scrapper.supreme_court_spain import SupremeCourtSpain
 
 config = configparser.ConfigParser()
 config.read("config.ini")
@@ -12,20 +10,18 @@ area:str='courts'
 source_SpanishSupremeCourt:str = 'SpanishSupremeCourt'
 source_USASupremeCourt:str = 'USASupremeCourt'
 
-#Something like data/json_data.json
+#Something like data/SpanishSupremeCourt.json
 output_path_SpanishSupremeCourt:str = '{}{}{}'.format(config['EXTRA']['OutputPath'],source_SpanishSupremeCourt,'.json')
 output_path_USASupremeCourt:str = '{}{}_{}{}'.format(config['EXTRA']['OutputPath'],source_USASupremeCourt,area,'.json')
 
 #Get some cases
-returned_data = SupremeCourtSpain(source_SpanishSupremeCourt,case_ids).get_data()
-print(returned_data)
+print(SupremeCourtSpain(source_SpanishSupremeCourt,case_ids).get_data())
 
 #Save cases as JSON
 SupremeCourtSpain(source_SpanishSupremeCourt,case_ids).save_data('json',output_path_SpanishSupremeCourt)
 
 #Get some cases
-returned_data = SupremeCourtUnitedStates(source_USASupremeCourt,area).get_data()
-print(returned_data)
+print(SupremeCourtUnitedStates(source_USASupremeCourt,area).get_data())
 
 SupremeCourtUnitedStates(source_USASupremeCourt,area).save_data('json',output_path_USASupremeCourt)
 
